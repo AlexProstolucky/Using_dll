8b326a085c7a10329dddab6b35704967970ae9de
AlexProstolucky <alexprostolucky@gmail.com>
2023-10-15 21:55:56 +0300

    Добавьте файлы проекта.

---
+++ Using_dll/Program.cs
@@ -0,0 +1,13 @@
+﻿using Rahunok_class;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Rahunok rah = new Rahunok(0, 0, 0, 0, 0,"");
+        rah.info();
+        rah.cash_app(1000);
+        rah.info();
+        rah.cash_down(900);
+        rah.info();
+    }
+}
